c41c8eead8fe903a4789f50acd72e4755a41026e
Vivek Kavala <vivek43143@gmail.com>
2026-02-17 22:11:30 +0530

    Updated

--- apps/backend/src/restaurants/restaurants.service.ts
+++ apps/backend/src/restaurants/restaurants.service.ts
@@ -3,7 +3,7 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { Role, Region } from '@prisma/client';
 
 export interface AuthenticatedUser {
